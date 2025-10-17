6ba28359bfb5d0f6b4b7c5ea5e756d22de618200
susxel <cbaruch@protonmail.com>
2025-10-17 18:35:05 +0200

    added caught-indicator on battle screen

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -1880,6 +1880,23 @@ DrawEnemyHUDAndHPBar:
 	lb bc, 4, 12
 	call ClearScreenArea
 	callfar PlaceEnemyHUDTiles
+	push hl
+	ld a, [wEnemyMonSpecies2]
+	ld [wPokedexNum], a
+	callfar IndexToPokedex
+	ld a, [wPokedexNum]
+	dec a
+	ld c, a
+	ld b, FLAG_TEST
+	ld hl, wPokedexOwned
+	predef FlagActionPredef
+	ld a, c
+	and a
+	jr z, .notOwned
+	hlcoord 10, 1
+	ld [hl], $72
+.notOwned
+	pop hl
 	ld de, wEnemyMonNick
 	hlcoord 1, 0
 	call CenterMonName
